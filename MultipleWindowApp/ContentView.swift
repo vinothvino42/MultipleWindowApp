33feef6f7757c07f682b050563d315637b09c1d9
vinothvino42 <vinothvino42@gmail.com>
2024-02-15 20:35:34 +0530

    Initial Commit

---
+++ MultipleWindowApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MultipleWindowApp
+//
+//  Created by Vinoth Vino on 15/02/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ MultipleWindowApp/MultipleWindowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleWindowAppApp.swift
+//  MultipleWindowApp
+//
+//  Created by Vinoth Vino on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipleWindowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
